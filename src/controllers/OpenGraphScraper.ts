8eb9d4f9db7a14ae9814e73836b9782b27b9701b
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2024-01-28 03:05:57 -0500

    Quick commit

--- src/controllers/OpenGraphScraper.ts
+++ src/controllers/OpenGraphScraper.ts
@@ -1,6 +1,6 @@
 import ogs from 'open-graph-scraper';
+import { userAgent } from '@/constants';
 
-const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36';
 const options = { url: 'https://mvp.microsoft.com/en-US/studentambassadors/profile/ff9d3bb7-25f6-477c-875d-37904a0c6280', fetchOptions: { headers: { 'user-agent': userAgent } } }
 
 ogs(options)
